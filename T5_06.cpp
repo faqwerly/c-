9204c2692511bd5f2cd74512d25ff6a7ffd9c968
faqwerly <100813087+faqwerly@users.noreply.github.com>
2022-03-31 10:46:51 +0800

    Add files via upload

---
+++ T5_06.cpp
@@ -0,0 +1,21 @@
+/*这是一个多行注释
+*文件名：T5_06.cpp
+*作者：Yitong Han
+*邮箱：H2101870137@163.com
+*日期：2022.3.26
+*学院：School of Computer Information and Engineering
+*功能：整数求平均值 
+*/ #include <iostream>
+using namespace std;
+int main()
+{
+	int i,a,num=0,b=0;
+	for(i=1; i=1;i++ )
+	{
+		cin>>a;
+		    if(9999==a)
+		          {break;}
+	     	else {num=num+a;b++;}
+	}
+	cout<<"AVG is "<<num/b<<endl;
+}
